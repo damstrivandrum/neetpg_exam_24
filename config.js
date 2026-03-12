d35f90777dd053e874b6ce07a8f0372867a07386
damstrivandrum <aajalms@gmail.com>
2026-03-12 22:35:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.API_URL =
   "https://script.google.com/macros/s/AKfycbyrR6uJDuFZBkbrtbjiTuIrKB31alvPWVmz4znmccpqPR_u1TLIqxYfziq9QJObdWBL/exec";
 
 // ✅ Exam settings
-window.EXAM_DURATION_MIN = 60;
+window.EXAM_DURATION_MIN = 180;
 window.MARKS_CORRECT = 4;
 window.MARKS_WRONG = -1;
 
